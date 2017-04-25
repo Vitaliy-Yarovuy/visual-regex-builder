f568741f7063d77d9e15883922e19000cf710a04
Vitaliy-Yarovuy <vitaliy.yxz@gmail.com>
2017-04-26 00:13:18 +0300

    add working drag and drop functional

--- src/app/workspace/workspace.component.ts
+++ src/app/workspace/workspace.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {ChangeDetectorRef, Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {RegexBuilderService} from '../core/regex-builder.service';
 import {Block, BlockType} from '../block-models/regex-block-models';
 import {DragulaService} from 'ng2-dragula';
@@ -23,27 +23,42 @@ export class WorkspaceComponent implements OnInit, OnChanges {
     {type: BlockType.EndOfLine, values: []}
   ];
 
-  constructor(public regexBuilderService: RegexBuilderService, public dragulaService: DragulaService) {
+  constructor(public regexBuilderService: RegexBuilderService, public dragulaService: DragulaService, public ref: ChangeDetectorRef) {
 
   }
 
   ngOnInit() {
+    const isSource = (el) => el.classList.contains('for-copy');
+    const buildBlockFromSource = (el) => ({type: el.getAttribute('ng-reflect-type'), values: []});
+
+    this.dragulaService.setOptions('drag-bag', {
+      copy:  isSource,
+    });
 
     this.dragulaService.drag.subscribe((args) => {
       const [bag, element, parent] = args;
-      const index = indexOf(parent.children, element);
-      const item = index + 1 ? this.blocks[index] : null;
-      console.log('drag:', item, args);
-      this.selectedBlock = item;
+      if(!isSource(element)){
+        const index = indexOf(parent.children, element);
+        const item = index + 1 ? this.blocks[index] : null;
+        console.log('drag:', item, args);
+        this.selectedBlock = item;
+      }
     });
 
     this.dragulaService.drop.subscribe((args) => {
       const [bag, element, parent] = args;
-      const index = indexOf(parent.children, element);
-      const oldIndex = this.blocks.indexOf(this.selectedBlock);
-      this.blocks.splice(oldIndex, 1);
-      this.blocks.splice(index, 0, this.selectedBlock);
-      this.selectedBlock = null;
+      if(isSource(element)){
+        const index = indexOf(parent.children, element);
+        this.blocks.splice(index, 0, buildBlockFromSource(element));
+        element.remove();
+      } else {
+        const index = indexOf(parent.children, element);
+        const oldIndex = this.blocks.indexOf(this.selectedBlock);
+        this.blocks.splice(oldIndex, 1);
+        this.blocks.splice(index, 0, this.selectedBlock);
+        this.selectedBlock = null;
+      }
+
     });
 
     this.regexBuilderService.generateRegex(this.blocks);
