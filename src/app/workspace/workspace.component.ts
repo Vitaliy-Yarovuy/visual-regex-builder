422f170943d94532ec4c20bca4704b0946badcac
Vitaliy-Yarovuy <vitaliy.yxz@gmail.com>
2017-04-25 17:31:49 +0300

    add sortable items

--- src/app/workspace/workspace.component.ts
+++ src/app/workspace/workspace.component.ts
@@ -1,11 +1,14 @@
 import {Component, OnChanges, OnInit, SimpleChanges, ViewChildren} from '@angular/core';
 import {RegexBuilderService} from '../core/regex-builder.service';
 import {getRegexBlock, BlockType} from '../block-models/models';
+import {DragulaService} from 'ng2-dragula';
 
 interface Block {
   type: BlockType;
   values: Array<string | number>;
 }
+
+const indexOf = (arr: any, item: any) => ([].indexOf.call(arr, item));
 
 @Component({
   selector: 'cg-workspace',
@@ -14,16 +17,36 @@ interface Block {
 })
 export class WorkspaceComponent implements OnInit, OnChanges {
 
-  blocks: Array<Block> = [
+  public selectedBlock: Block|null = null;
+
+  public blocks: Array<Block> = [
     { type: BlockType.StartOfLine, values: []},
     { type: BlockType.Text, values: ['http']},
     { type: BlockType.Maybe, values: ['s']}
   ];
 
-  constructor(public regexBuilderService: RegexBuilderService) {
+  constructor(public regexBuilderService: RegexBuilderService, public dragulaService: DragulaService) {
+
   }
 
   ngOnInit() {
+
+    this.dragulaService.drag.subscribe((args) => {
+      const  [bag, element, parent] = args;
+      const index = indexOf(parent.children, element);
+      const item = index + 1 ? this.blocks[index] : null;
+      console.log('drag:', item, args);
+      this.selectedBlock = item;
+    });
+
+    this.dragulaService.drop.subscribe((args) => {
+      const [bag, element, parent] = args;
+      const index = indexOf(parent.children, element);
+      const oldIndex = this.blocks.indexOf(this.selectedBlock);
+      this.blocks.splice(oldIndex, 1);
+      this.blocks.splice(index, 0, this.selectedBlock);
+      this.selectedBlock = null;
+    });
   }
 
   ngOnChanges(changes: SimpleChanges): void {
