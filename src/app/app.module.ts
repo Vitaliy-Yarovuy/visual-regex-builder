fbfca82e80a55832e85b13abab491baaf9ef6999
Alexander Kabanov <sanya-terror@Alexanders-MacBook-Pro.local>
2017-04-25 15:21:17 +0300

    Removed redundant markup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 import {MaterialModule} from '@angular/material';
-import { DragulaModule } from 'ng2-dragula/ng2-dragula';
+import {DragulaModule} from 'ng2-dragula/ng2-dragula';
 
-import { AppComponent } from './app.component';
-import { BlocksContainerComponent } from './blocks-container/blocks-container.component';
-import { WorkspaceComponent } from './workspace/workspace.component';
-import { OutputComponent } from './output/output.component';
-import { TestComponent } from './test/test.component';
+import {AppComponent} from './app.component';
+import {BlocksContainerComponent} from './blocks-container/blocks-container.component';
+import {WorkspaceComponent} from './workspace/workspace.component';
+import {OutputComponent} from './output/output.component';
+import {TestComponent} from './test/test.component';
 import {RegexBuilderService} from './core/regex-builder.service';
 
 @NgModule({
@@ -30,4 +30,5 @@ import {RegexBuilderService} from './core/regex-builder.service';
   providers: [RegexBuilderService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
