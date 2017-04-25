73d6d3f7e155752befb8285fcbd88a55b79060aa
Ne4istb <anechytailov@gmail.com>
2017-04-25 11:05:31 +0300

    Init core components

---
+++ src/app/core/regex-block/regex-block.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-regex-block',
+  templateUrl: './regex-block.component.html',
+  styleUrls: ['./regex-block.component.css']
+})
+export class RegexBlockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
