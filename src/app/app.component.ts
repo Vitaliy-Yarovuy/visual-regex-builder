4c61fb6e8b899a141c4f1db10d66e0670e0e11dc
Vitaliy-Yarovuy <vitaliy.yxz@gmail.com>
2017-04-25 12:02:37 +0300

    include DragulaModule into project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+    '../../node_modules/dragula/dist/dragula.css',
+    './app.component.css'
+  ]
 })
 export class AppComponent {
   title = 'app works!';
