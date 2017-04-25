e58939aea20d0b677c1bfd210cea64ced202b1c9
Alexander Kabanov <sanya-terror@Alexanders-MacBook-Pro.local>
2017-04-25 13:07:02 +0300

    Added Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Wolters Kluwer Code Games';
+  subtitle = 'Regular Expressions Builder';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {MaterialModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { RegexBlockComponent } from './core/regex-block/regex-block.component';
@@ -20,7 +21,8 @@ import { OutputComponent } from './output/output.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
