90d276629bdb8eefe132e4bc860c3e81f1d689fa
Ne4istb <anechytailov@gmail.com>
2017-04-26 16:26:34 +0300

    fix testing area data

--- src/app/testing-area/testing-area.component.ts
+++ src/app/testing-area/testing-area.component.ts
@@ -12,15 +12,15 @@ export class TestingAreaComponent implements OnInit {
   timeout;
 
   search: RegExp;
-  text = `<div>Sample text for testing: </div>
-<div>abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ</div> 
-<div>0123456789 _+-.,!@#$%^&*();\/|<>"' </div>
-<div>12345 -98.7 3.141 .6180 9,000 +42 </div>
-<div>555.123.4567	+1-(800)-555-2468</div>
-<div>foo@demo.net	bar.ba@test.co.uk</div>
-<div>www.demo.com	http://foo.co.uk/</div>
-<div>http://regexr.com/foo.html?q=bar</div>
-<div>https://mediatemple.net</div>`;
+  text = `Sample text for testing: 
+          abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ 
+          0123456789 _+-.,!@#$%^&*();\/|<>"' 
+          12345 -98.7 3.141 .6180 9,000 +42 
+          555.123.4567	+1-(800)-555-2468
+          foo@demo.net	bar.ba@test.co.uk
+          www.demo.com	http://foo.co.uk/
+          http://regexr.com/foo.html?q=bar
+          https://mediatemple.net`;
 
   constructor(private regexBuilderService: RegexBuilderService, private highlightPipe: HighlightPipe) {
   }
