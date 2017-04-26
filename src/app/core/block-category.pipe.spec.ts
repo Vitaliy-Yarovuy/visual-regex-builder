103b738f2bb46e30e99c453932450f0254e5c69b
Ne4istb <anechytailov@gmail.com>
2017-04-26 16:11:50 +0300

    New version

--- src/app/core/block-category.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BlockCategoryPipe } from './block-category.pipe';
-
-describe('BlockCategoryPipe', () => {
-  it('create an instance', () => {
-    const pipe = new BlockCategoryPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/core/block-types.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BlockTypesPipe } from './block-types.pipe';
-
-describe('BlockTypesPipe', () => {
-  it('create an instance', () => {
-    const pipe = new BlockTypesPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
